15f2f94db3dc6ee983b3301104f25c76ea51da46
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-10 19:12:59 +0900

    ガチャ結果のクラスを作った 画面遷移を設定した ガチャ結果画面に部品を配置して関連付けした

---
+++ GachTech/ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  GachTech
+//
+//  Created by Rika Sumitomo on 2019/04/10.
+//  Copyright © 2019 Rika Sumitomo. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    //一番後ろの画像
+    @IBOutlet var haikeiImageView: UIImageView!
+    //モンスターを表示する画像
+    @IBOutlet var monsterImageView: UIImageView!
+    //モンスター画像を保存しておく入れる
+    var monsterArray: [UIImage]!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
