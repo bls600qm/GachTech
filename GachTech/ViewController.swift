07c30e30a054e4b1938446df2fa5a4a47c108a46
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-10 18:55:32 +0900

    背景，ボタンを配置した 回すを押した時に次の画面に行くように 戻るを押した時に戻るようにした

--- GachTech/ViewController.swift
+++ GachTech/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func gacha(){
+        self.performSegue(withIdentifier: "result", sender: nil)
+    }
 
 }
 
